b353722ec40e70900f68d073b76ad3813b76064a
Nyasha Chirinda <chrndah@gmail.com>
2023-01-30 15:40:58 +0200

    add repository layer

---
+++ src/main/java/com/assesment/droneapplication/repository/DroneRepository.java
@@ -0,0 +1,9 @@
+package com.assesment.droneapplication.repository;
+
+import com.assesment.droneapplication.model.Drone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface DroneRepository extends JpaRepository<Drone, UUID> {
+}
---
+++ src/main/java/com/assesment/droneapplication/repository/MedicationRepository.java
@@ -0,0 +1,9 @@
+package com.assesment.droneapplication.repository;
+
+import com.assesment.droneapplication.model.Medication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MedicationRepository extends JpaRepository<Medication, UUID> {
+}
