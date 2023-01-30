572cbc94cdd02e7454816c6439b12f11b3e95ede
Nyasha Chirinda <chrndah@gmail.com>
2023-01-30 15:05:15 +0200

    add model classes

---
+++ src/main/java/com/assesment/droneapplication/model/Drone.java
@@ -0,0 +1,73 @@
+package com.assesment.droneapplication.model;
+
+import com.assesment.droneapplication.model.enums.DroneModel;
+import com.assesment.droneapplication.model.enums.DroneState;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+
+/**
+ * @author Nyasha Chirinda - 30/01/2023
+ */
+
+@Entity
+@Table(name = "drones")
+public class Drone extends BaseEntity {
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String serialNumber;
+
+    @Enumerated
+    @Column(nullable = false)
+    private DroneModel model;
+
+    private double weightLimit;
+
+    private double batteryCapacity;
+
+    @Enumerated
+    @Column(nullable = false)
+    private DroneState state;
+
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public DroneModel getModel() {
+        return model;
+    }
+
+    public void setModel(DroneModel model) {
+        this.model = model;
+    }
+
+    public double getWeightLimit() {
+        return weightLimit;
+    }
+
+    public void setWeightLimit(double weightLimit) {
+        this.weightLimit = weightLimit;
+    }
+
+    public double getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(double batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public DroneState getState() {
+        return state;
+    }
+
+    public void setState(DroneState state) {
+        this.state = state;
+    }
+}
---
+++ src/main/java/com/assesment/droneapplication/model/Medication.java
@@ -0,0 +1,55 @@
+package com.assesment.droneapplication.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ * @author Nyasha Chirinda - 30/01/2023
+ */
+
+@Entity
+@Table(name = "medications")
+public class Medication extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+   private double weight;
+
+   private String code;
+
+   private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
