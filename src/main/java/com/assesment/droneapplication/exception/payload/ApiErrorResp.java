c45d3ca8b58d32db7e34467a02371f9bde48f374
Nyasha Chirinda <chrndah@gmail.com>
2023-01-31 12:14:26 +0200

    add global exception handler

---
+++ src/main/java/com/assesment/droneapplication/exception/payload/ApiErrorResp.java
@@ -0,0 +1,47 @@
+package com.assesment.droneapplication.exception.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Nyasha Chirinda - 31/01/2023
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiErrorResp {
+
+    private LocalDateTime timestamp;
+    private String source;
+    private int status;
+    private String message;
+    private String path;
+    private Map<String, String> errors = new HashMap<>();
+
+    public ApiErrorResp(int status, String message, String error, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.source = "drone-service";
+        this.status = status;
+        this.message = message;
+        this.path = path;
+        this.errors.put("error", error);
+    }
+
+    public ApiErrorResp(int status, String message, Map<String, String> errors, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.source = "drone-service";
+        this.status = status;
+        this.message = message;
+        this.path = path;
+        this.errors = errors;
+    }
+
+}
