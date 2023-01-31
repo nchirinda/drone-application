c4feb99ed775c1c229b3da00b154ef1fcedd1c4d
Nyasha Chirinda <chrndah@gmail.com>
2023-01-31 19:02:19 +0200

    add util classes and handlers

---
+++ src/main/java/com/assesment/droneapplication/model/payload/ApiSuccessResp.java
@@ -0,0 +1,20 @@
+package com.assesment.droneapplication.model.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author Chirinda Nyasha Dell 23/11/2021
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiSuccessResp {
+
+    private String message;
+
+}
