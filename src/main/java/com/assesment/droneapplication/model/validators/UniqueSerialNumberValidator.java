b2902027260b8e3de7b0872a43bb645435a34053
Nyasha Chirinda <chrndah@gmail.com>
2023-01-31 12:13:56 +0200

    add validator for unique serial number

---
+++ src/main/java/com/assesment/droneapplication/model/validators/UniqueSerialNumberValidator.java
@@ -0,0 +1,24 @@
+package com.assesment.droneapplication.model.validators;
+
+import com.assesment.droneapplication.repository.DroneRepository;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+/**
+ * @author Nyasha Chirinda - 31/01/2023
+ */
+
+public class UniqueSerialNumberValidator implements ConstraintValidator<UniqueSerialNumber, String> {
+
+    private final DroneRepository droneRepository;
+
+    public UniqueSerialNumberValidator(DroneRepository droneRepository) {
+        this.droneRepository = droneRepository;
+    }
+
+    @Override
+    public boolean isValid(String username, ConstraintValidatorContext context) {
+        return !droneRepository.existsBySerialNumber(username);
+    }
+}
+
